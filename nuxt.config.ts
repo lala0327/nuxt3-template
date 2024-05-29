687296974578306f5a67906e5ff201cc1f453a6c
lala <lala@program.com.tw>
2024-05-29 16:53:48 +0800

    add eslint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,7 @@
+import { defineNuxtConfig } from 'nuxt/config';
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: ["@nuxt/eslint"]
 })
